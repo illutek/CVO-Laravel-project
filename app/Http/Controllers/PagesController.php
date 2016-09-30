07a57f403ef34061d466a54cb98e2102df9baf22
stefan <stefan@illutek.be>
2016-09-30 11:39:39 +0200

    logic van klas verhuizen van web.php naar PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,8 +8,10 @@
 
 namespace App\Http\Controllers;
 
-class PagesController extends Controller {
-    public function getIndex() {
+class PagesController extends Controller
+{
+    public function getIndex()
+    {
         /**
          * kan ook met pages/welcome ipv pages.welcome
          * iets meer Object oriented OOP
@@ -17,7 +19,34 @@ class PagesController extends Controller {
         return view('pages.welcome');
     }
 
-    public function getOldabout() {
+    public function getAbout()
+    {
+        return view('pages.about');
+    }
+
+    public function getContact()
+    {
+        return view('pages.contact');
+    }
+
+    public function getKlas()
+    {
+        $klasgenoten = [
+            'stefan',
+            'danny',
+            'davy',
+            'pascal',
+            'mohammet'
+        ];
+
+        return view('pages.klas')->with('klasgenoten', $klasgenoten);
+    }
+
+    /**
+     * onderstaande is maar als oefening
+     */
+    public function getOldabout()
+    {
         $first = 'Stefan';
         $last = 'Vandenborne';
         $fullname = $first . " " . $last;
@@ -34,13 +63,5 @@ class PagesController extends Controller {
          * De laatst gebruikte via een array $data
          */
         return view('pages.oldabout')->withData($data);
-    }
-
-    public function getAbout() {
-        return view('pages.about');
-    }
-
-    public function getContact() {
-        return view('pages.contact');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,4 @@ Route::get('contact', 'PagesController@getContact');
 Route::get('about', 'PagesController@getAbout');
 Route::get('oldabout', 'PagesController@getOldabout');
 Route::get('/', 'PagesController@getIndex');
-
-Route::get('/klas' , function() {
-    $klasgenoten = [
-        'stefan',
-        'danny',
-        'davy',
-        'pascal',
-        'mohammet'
-    ];
-    //return view('pages/klas', ['klasgenoten'=>$klasgenoten]);
-    return view('pages/klas') ->with('klasgenoten', $klasgenoten);
-});
+Route::get('klas', 'PagesController@getKlas');
